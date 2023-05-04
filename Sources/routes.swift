dc5d6235299245a7b6f1b538571be45adaf17406
kPherox <admin@mail.kr-kp.com>
2023-05-05 04:28:31 +0900

    fix: last update time is completed polling only

--- Sources/routes.swift
+++ Sources/routes.swift
@@ -3,9 +3,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
   app.get { req -> View in
-    let latestPolling = try await PollingHistory.query(on: req.db).filter(\.$failed == false).sort(
-      \.$insertedAt, .descending
-    ).field(\.$createdAt).field(\.$insertedAt).first()
+    let latestPolling = try await PollingHistory.query(on: req.db).filter(\.$failed == false)
+      .filter(\.$completed == true).sort(\.$insertedAt, .descending).field(\.$createdAt).field(
+        \.$insertedAt
+      ).first()
     return try await req.view.render(
       "index",
       ["latestImported": latestPolling?.createdAt, "lastImport": latestPolling?.insertedAt])
