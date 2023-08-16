bf69e718a7e8234a32d23087314fb1b2dc2ef87a
kPherox <kphrx@kpherox.dev>
2023-08-16 22:56:52 +0900

    fix: wrong changes 🤯 (#16)

--- Sources/Migrations/AddFailedColumnToPollingHistoryTable.swift
+++ Sources/Migrations/AddFailedColumnToPollingHistoryTable.swift
@@ -3,7 +3,6 @@ import Fluent
 struct AddFailedColumnToPollingHistoryTable: AsyncMigration {
   func prepare(on database: Database) async throws {
     try await database.schema("polling_history")
-      .field("failed", .bool)
       .field("failed", .bool, .required, .custom("DEFAULT false"))
       .update()
   }
