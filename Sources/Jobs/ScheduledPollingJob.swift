7e65d6316e0315dda1c3028e142c9d7e72293d8b
kPherox <kphrx@kpherox.dev>
2023-12-11 22:07:35 +0900

    fix: POLLING_MAX_COUNT convert to UInt

--- Sources/Jobs/ScheduledPollingJob.swift
+++ Sources/Jobs/ScheduledPollingJob.swift
@@ -10,7 +10,7 @@ struct ScheduledPollingJob: AsyncScheduledJob {
       try await history.create(on: app.db)
       try await app.queues.queue.dispatch(
         PollingPlcServerExportJob.self,
-        .init(after: after, count: Environment.get("POLLING_MAX_COUNT") ?? 1000, history: history)
+        .init(after: after, count: Environment.get("POLLING_MAX_COUNT").flatMap(UInt.init(_:)) ?? 1000, history: history)
       )
     } catch {
       app.logger.report(error: error)
