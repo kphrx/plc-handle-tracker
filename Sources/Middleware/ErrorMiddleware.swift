d815ca43c89531da96523b864d618b5e6ef1841c
kPherox <kphrx@kpherox.dev>
2023-12-24 21:50:28 +0900

    refactor: remove ternary conditional op from ErrorMiddleware

--- Sources/Middleware/ErrorMiddleware.swift
+++ Sources/Middleware/ErrorMiddleware.swift
@@ -13,46 +13,54 @@ struct ErrorMiddleware: AsyncMiddleware {
     do {
       return try await next.respond(to: req)
     } catch {
-      return await self.handle(error: error, for: req)
+      req.logger.report(error: error)
+      let (status, reason, context) = self.handle(error: error, route: req.route?.description ?? "")
+      return await self.response(status: status, context: context, reason: reason, for: req)
     }
   }
 
-  private func handle(error: Error, for req: Request) async -> Response {
-    let status: HTTPStatus
-    let reason: String?
-    switch error {
-    case let abort as AbortError:
-      status = abort.status
-      reason = abort.reason != status.reasonPhrase ? abort.reason : nil
-    default:
-      status = .internalServerError
-      reason = self.environment.isRelease ? "Something went wrong." : String(describing: error)
-    }
+  private func handle(error: Error, route: String) -> (HTTPStatus, String?, ErrorContext) {
+    let status: HTTPStatus =
+      switch error {
+      case let abort as AbortError: abort.status
+      default: .internalServerError
+      }
+    let reason: String? =
+      switch error {
+      case let abort as AbortError:
+        if abort.reason != status.reasonPhrase { abort.reason } else { nil }
+      default:
+        if self.environment.isRelease { "Something went wrong." } else { String(describing: error) }
+      }
     let context = ErrorContext(
-      title: "\(status.code) \(status.reasonPhrase)", route: req.route?.description ?? "",
-      reason: reason)
-    let res: Response
+      title: "\(status.code) \(status.reasonPhrase)", route: route, reason: reason)
+    return (status, reason, context)
+  }
+
+  private func response(
+    status: HTTPStatus, context: ErrorContext, reason: String?, for req: Request
+  ) async -> Response {
     do {
-      res = try await self.render(for: req, code: status.code, context: context)
+      return try await self.render(status: status, context: context, for: req)
     } catch {
-      res = .init(
+      return .init(
+        status: status,
+        headers: [HTTPHeaders.Name.contentType.description: "text/plain; charset=utf-8"],
         body: .init(
           string: "Oops: \(reason ?? "Something went wrong.")",
           byteBufferAllocator: req.byteBufferAllocator))
-      res.headers.add(name: .contentType, value: "text/plain; charset=utf-8")
     }
-    res.status = status
-    req.logger.report(error: error)
-    return res
   }
 
-  private func render(for req: Request, code statusCode: UInt, context: ErrorContext) async throws
+  private func render(status: HTTPStatus, context: ErrorContext, for req: Request) async throws
     -> Response
   {
     do {
-      return try await req.view.render("error/\(statusCode)", context).encodeResponse(for: req)
+      return try await req.view.render("error/\(status.code)", context).encodeResponse(
+        status: status, for: req)
     } catch {
-      return try await req.view.render("error/default", context).encodeResponse(for: req)
+      return try await req.view.render("error/default", context).encodeResponse(
+        status: status, for: req)
     }
   }
 }
