236cdfddb87bab329af7aa956e7cdcb9b859e2f3
kPherox <kphrx@kpherox.dev>
2024-02-10 00:06:05 +0900

    style: formatting
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Sources/Jobs/jobs.swift
+++ Sources/Jobs/jobs.swift
@@ -11,25 +11,30 @@ func jobs(_ app: Application) {
   let pollingStart = Environment.get("POLLING_START_AT_MINUTES").flatMap(Int.init(_:)) ?? 20
   let afterRecovery = Environment.get("AFTER_POLLING_RECOVERLY_MINUTES").flatMap(Int.init(_:)) ?? 15
   app.queues.scheduleEvery(ScheduledPollingJob(), stride: pollingInterval, from: pollingStart)
-  app.queues.scheduleEvery(ScheduledPollingRecoveryJob(), stride: pollingInterval, from: pollingStart + afterRecovery)
+  app.queues.scheduleEvery(
+    ScheduledPollingRecoveryJob(), stride: pollingInterval, from: pollingStart + afterRecovery)
 
-  let disableCleanupHistory = Environment.get("DISABLE_POLLING_HISTORY_CLEANUP").flatMap({
-    switch $0.lowercased() {
-        case "true", "t", "yes", "y":
-            return true
-        case "false", "f", "no", "n", "":
-            return false
-        default:
-            if let int = Int($0) {
-                return int != 0
-            }
-            return nil
+  let disableCleanupHistory =
+    Environment.get("DISABLE_POLLING_HISTORY_CLEANUP").flatMap({
+      switch $0.lowercased() {
+      case "true", "t", "yes", "y":
+        return true
+      case "false", "f", "no", "n", "":
+        return false
+      default:
+        if let int = Int($0) {
+          return int != 0
         }
-  }) ?? false
-  if (disableCleanupHistory) {
+        return nil
+      }
+    }) ?? false
+  if disableCleanupHistory {
     return
   }
-  let cleanupInterval = Environment.get("POLLING_HISTORY_CLEANUP_INTERVAL").flatMap(Int.init(_:)) ?? 15
-  let cleanupStart = Environment.get("POLLING_HISTORY_CLEANUP_START_AT_MINUTES").flatMap(Int.init(_:)) ?? 10
-  app.queues.scheduleEvery(ScheduledPollingHistoryCleanupJob(), stride: cleanupInterval, from: cleanupStart)
+  let cleanupInterval =
+    Environment.get("POLLING_HISTORY_CLEANUP_INTERVAL").flatMap(Int.init(_:)) ?? 15
+  let cleanupStart =
+    Environment.get("POLLING_HISTORY_CLEANUP_START_AT_MINUTES").flatMap(Int.init(_:)) ?? 10
+  app.queues.scheduleEvery(
+    ScheduledPollingHistoryCleanupJob(), stride: cleanupInterval, from: cleanupStart)
 }
