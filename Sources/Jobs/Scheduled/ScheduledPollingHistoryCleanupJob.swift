42c87e9e6524609cb0a88028c1a6a24f44c2b16b
kPherox <kphrx@kpherox.dev>
2024-09-28 23:50:25 +0900

    fix: use same filters of polling_history for cleanup as for polling (#86)

--- Sources/Jobs/Scheduled/ScheduledPollingHistoryCleanupJob.swift
+++ Sources/Jobs/Scheduled/ScheduledPollingHistoryCleanupJob.swift
@@ -8,16 +8,15 @@ struct ScheduledPollingHistoryCleanupJob: AsyncScheduledJob {
       try await PollingJobStatus.query(on: app.db).filter(\.$status ~~ [.success, .banned]).delete()
       let errorOrRunnings = try await PollingJobStatus.query(on: app.db).all(\.$history.$id)
       guard
-        let insertedAt = try await PollingHistory.query(on: app.db).filter(\.$failed == false)
-          .group(.or, { $0.filter(\.$completed == true).filter(\.$id !~ errorOrRunnings) }).sort(
-            \.$insertedAt, .descending
-          ).range(5...).limit(1).all(\.$insertedAt).first
+        let insertedAt = try await PollingHistory.queryCompleted(on: app.db, errorOrRunnings).sort(
+          \.$insertedAt, .descending
+        ).range(5...).limit(1).all(\.$insertedAt).first
       else {
         return
       }
-      try await PollingHistory.query(on: app.db).filter(\.$failed == false).group(.or) {
-        $0.filter(\.$completed == true).filter(\.$id !~ errorOrRunnings)
-      }.filter(\.$insertedAt < insertedAt).delete()
+      try await PollingHistory.queryCompleted(on: app.db, errorOrRunnings).filter(
+        \.$insertedAt < insertedAt
+      ).delete()
     } catch {
       app.logger.report(error: error)
     }
