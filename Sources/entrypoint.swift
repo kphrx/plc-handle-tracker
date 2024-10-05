845ee0d6b771c5da07f668bf1b383e55833e8d13
kPherox <kphrx@kpherox.dev>
2024-10-05 19:57:57 +0000

    feat: enable to install NIO as Swift Concurrency

--- Sources/entrypoint.swift
+++ Sources/entrypoint.swift
@@ -1,8 +1,7 @@
 import Logging
+import NIOCore
+import NIOPosix
 import Vapor
-
-// import NIOCore
-// import NIOPosix
 
 @main
 enum Entrypoint {
@@ -16,8 +15,11 @@ enum Entrypoint {
     // You can enable it if you'd like to reduce the amount of context switching between NIO and Swift Concurrency.
     // Note: this has caused issues with some libraries that use `.wait()` and cleanly shutting down.
     // If enabled, you should be careful about calling async functions before this point as it can cause assertion failures.
-    // let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
-    // app.logger.debug("Tried to install SwiftNIO's EventLoopGroup as Swift's global concurrency executor", metadata: ["success": .stringConvertible(executorTakeoverSuccess)])
+    let executorTakeoverSuccess =
+      NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
+    app.logger.debug(
+      "Tried to install SwiftNIO's EventLoopGroup as Swift's global concurrency executor",
+      metadata: ["success": .stringConvertible(executorTakeoverSuccess)])
 
     do {
       try await configure(app)
