dfff0dc271a9c035defdd47d7bdca669e627eea4
kPherox <kphrx@kpherox.dev>
2024-10-18 18:45:36 +0000

    refactor: use polymophic parameters for error handling

--- Sources/Middleware/ErrorMiddleware.swift
+++ Sources/Middleware/ErrorMiddleware.swift
@@ -14,31 +14,28 @@ struct ErrorMiddleware: AsyncMiddleware {
       return try await next.respond(to: req)
     } catch {
       req.logger.report(error: error)
-      let (status, reason, context) = self.handle(error: error, route: req.route?.description ?? "")
+      let (status, reason) = self.handle(error: error)
+      let context = ErrorContext(
+        title: "\(status.code) \(status.reasonPhrase)", route: req.route?.description ?? "",
+        reason: reason)
       return await self.response(status: status, context: context, reason: reason, for: req)
     }
   }
 
-  private func handle(error: Error, route: String) -> (HTTPStatus, String?, ErrorContext) {
-    let status: HTTPStatus =
-      switch error {
-        case let abort as AbortError: abort.status
-        default: .internalServerError
-      }
-    let reason: String? =
-      switch error {
-        case let abort as AbortError:
-          if abort.reason != status.reasonPhrase { abort.reason } else { nil }
-        default:
-          if self.environment.isRelease {
-            "Something went wrong."
-          } else {
-            String(describing: error)
-          }
-      }
-    let context = ErrorContext(
-      title: "\(status.code) \(status.reasonPhrase)", route: route, reason: reason)
-    return (status, reason, context)
+  private func handle(error abort: AbortError) -> (HTTPStatus, String?) {
+    if abort.reason != abort.status.reasonPhrase {
+      (abort.status, abort.reason)
+    } else {
+      (abort.status, nil)
+    }
+  }
+
+  private func handle(error: Error) -> (HTTPStatus, String?) {
+    if self.environment.isRelease {
+      (.internalServerError, "Something went wrong.")
+    } else {
+      (.internalServerError, String(describing: error))
+    }
   }
 
   private func response(
