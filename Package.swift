86d97ee3349ae5d91fb4eb87dcf4dd11324973cc
kPherox <kphrx@kpherox.dev>
2025-05-11 02:10:55 +0900

    chore: bump macOS to 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "plc-handle-tracker",
   platforms: [
-    .macOS(.v10_15)
+    .macOS(.v13)
   ],
   dependencies: [
     .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
